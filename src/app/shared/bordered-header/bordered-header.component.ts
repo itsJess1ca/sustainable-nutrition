27ff141800d0faa1e9af15d8688f8f7b690fa454
Jordan Ide <jorrdandied@hotmail.com>
2016-11-06 00:56:49 +0000

    add simple rollover-animation

--- src/app/shared/bordered-header/bordered-header.component.ts
+++ src/app/shared/bordered-header/bordered-header.component.ts
@@ -3,9 +3,26 @@ import { Component, Input, OnInit, OnChanges, Renderer, ElementRef } from '@angu
 @Component({
   selector: '[bordered-header]',
   styleUrls: ['bordered-header.component.scss'],
-  template: `<ng-content></ng-content>`
+  template: `<div class="ripple"></div><ng-content></ng-content>`,
+  host: {
+    '(mouseover)': 'onMouseOver($event)',
+    '(mouseleave)': 'onMouseLeave($event)'
+  }
 })
 export class BorderedHeaderComponent {
+  isAlive: boolean = false;
   constructor(
+    private el: ElementRef,
+    private renderer: Renderer
   ) {}
+
+  onMouseOver(event): void {
+    this.renderer.setElementClass(this.el.nativeElement.querySelector('.ripple'), 'active', true);
+  }
+  onMouseLeave(event): void {
+    this.clearRipple();
+  }
+  clearRipple(): void {
+    this.renderer.setElementClass(this.el.nativeElement.querySelector('.ripple'), 'active', false);
+  }
 }
