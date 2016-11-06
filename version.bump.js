02687f4c65817f8152efce7270d28b2e4f47a961
Jordan Ide <jorrdandied@hotmail.com>
2016-11-06 00:55:11 +0000

    update all references to the app version

--- version.bump.js
+++ version.bump.js
@@ -3,9 +3,23 @@
 const pkgJson = require('./package.json');
 const fs = require('fs');
 
+
 const CURRENT_VERSION = pkgJson.version;
+
 
 fs.writeFile('./src/environments/version.ts', `export const APP_VERSION = "${CURRENT_VERSION}";`, (err) => {
   if (err) throw err;
-  console.log("It\'s Saved!");
+  console.log("Version environment variable updated.");
+});
+
+fs.readFile('./src/service-worker.js', {encoding: 'utf8'}, (err, data) => {
+  if (err) throw err;
+
+  const newData = data
+    .replace(/const VERSION = '[\S].*';/g, `const VERSION = '${CURRENT_VERSION}';`);
+
+  fs.writeFile('./src/service-worker.js', newData, (err) => {
+    if (err) throw err;
+    console.log("Service worker version updated.");
+  });
 });
