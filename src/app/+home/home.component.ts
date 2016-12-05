1f391eb894479026abfde1f008a7b34b5ac9c011
Jordan Ide <jorrdandied@hotmail.com>
2016-12-05 01:19:10 +0000

    update contentful service to actually trigger change detection

--- src/app/+home/home.component.ts
+++ src/app/+home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { ContentfulService } from '../shared/contentful.service';
 import { Observable } from 'rxjs';
 
@@ -6,31 +6,14 @@ import { Observable } from 'rxjs';
   selector: 'sn-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class HomeComponent implements OnInit {
   generalData: Observable<string> = this.content.getEntry('52yMQv1MOkQAKMueWEEcyM');
   testimonial: Observable<any> = this.content.getRandomTestimonial();
   coaches: Observable<any> = this.content.getEntries({'content_type': 'coaches'});
-  constructor(public content: ContentfulService, private _ref: ChangeDetectorRef) { }
+  constructor(public content: ContentfulService) { }
 
   ngOnInit() {
-    const interval = setInterval(() => {
-      if (this.generalData && this.generalData.source && this.generalData.source.value !== null) {
-        clearInterval(interval);
-      }
-      this._ref.markForCheck();
-    }, 100);
-    const interval2 = setInterval(() => {
-      if (this.testimonial && this.testimonial.source && this.testimonial.source.value !== null) {
-        clearInterval(interval2);
-      }
-      this._ref.markForCheck();
-    }, 100);
-    const interval3 = setInterval(() => {
-      if (this.coaches && this.coaches.source && this.coaches.source.value !== null) {
-        clearInterval(interval3);
-      }
-      this._ref.markForCheck();
-    }, 100);
   }
 }
