1433772d8f567e0c48f9fbaa0878c3455f77334c
Jordan Ide <jorrdandied@hotmail.com>
2017-01-12 07:19:33 +0000

    remove step from services fields

--- src/app/+services/services.component.ts
+++ src/app/+services/services.component.ts
@@ -6,31 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent implements OnInit {
+  // Thom: I removed the 'step' field - you can just use `index + 1` in your for loop, step isn't in the data :)
   services: any[] =
     [
       {
-        'step': 1,
         'image': 'handplatething.png',
         'title': 'nutrition plan',
-        'description' :'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ultricies nisi non risus convallis convallis. Aliquam erat volutpat. Curabitur efficitur sed enim a molestie.'
+        'description' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ultricies nisi non risus convallis convallis. Aliquam erat volutpat. Curabitur efficitur sed enim a molestie.'
       },
       {
-        'step': 2,
         'image': 'handplatething.png',
         'title': 'recipes',
-        'description' :'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ultricies nisi non risus convallis convallis. Aliquam erat volutpat. Curabitur efficitur sed enim a molestie.'
+        'description' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ultricies nisi non risus convallis convallis. Aliquam erat volutpat. Curabitur efficitur sed enim a molestie.'
       },
       {
-        'step': 3,
         'image': 'handplatething.png',
         'title': 'weekly educational updates',
-        'description' :'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ultricies nisi non risus convallis convallis. Aliquam erat volutpat. Curabitur efficitur sed enim a molestie.'
+        'description' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ultricies nisi non risus convallis convallis. Aliquam erat volutpat. Curabitur efficitur sed enim a molestie.'
       },
       {
-        'step': 4,
         'image': 'handplatething.png',
         'title': 'something else',
-        'description' :'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ultricies nisi non risus convallis convallis. Aliquam erat volutpat. Curabitur efficitur sed enim a molestie.'
+        'description' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ultricies nisi non risus convallis convallis. Aliquam erat volutpat. Curabitur efficitur sed enim a molestie.'
       }
     ];
   constructor() { }
