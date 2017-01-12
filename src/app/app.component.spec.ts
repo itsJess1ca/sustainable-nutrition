5857482f1e69a9d4493b7d67c8e386a041d937cd
Jordan Ide <jorrdandied@hotmail.com>
2017-01-12 07:34:20 +0000

    fix spec test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,11 +10,13 @@ import 'rxjs/add/operator/takeUntil';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { FooterModule } from './footer/footer.module';
 import { BrandLogoModule } from './brand-logo/brand-logo.module';
+import { SimpleNotificationsModule } from 'angular2-notifications';
 
 describe('App Component', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
+        SimpleNotificationsModule,
         BrandLogoModule,
         FooterModule,
         RouterTestingModule.withRoutes(routes)
