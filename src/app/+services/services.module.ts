f5ea33f71ba2dfc20ba979236d34ed57bd32488f
Jordan Ide <jorrdandied@hotmail.com>
2017-01-12 03:07:29 +0000

    fix service body text

--- src/app/+services/services.module.ts
+++ src/app/+services/services.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ServicesComponent } from './services.component';
 import { RouterModule } from '@angular/router';
 import { SERVICES_ROUTES } from './services.routes';
-import {PageTitleModule} from "../shared/page-title/page-title.module";
-import {ServiceComponent} from "./service/service.component";
-import {BorderedHeaderModule} from "../shared/bordered-header/bordered-header.module";
+import { PageTitleModule } from '../shared/page-title/page-title.module';
+import { ServiceComponent } from './service/service.component';
+import { BorderedHeaderModule } from '../shared/bordered-header/bordered-header.module';
+import { StripeFormModule } from '../shared/stripe-form/stripe-form.module';
 
 @NgModule({
   imports: [
+    StripeFormModule,
     BorderedHeaderModule,
     CommonModule,
     PageTitleModule,
