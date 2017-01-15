a0926640cf4ddb49cedf0f4bb1b3a3b5179628be
Jordan Ide <jorrdandied@hotmail.com>
2017-01-15 04:18:48 +0000

    fix linting

--- src/app/+service/service.routing.ts
+++ src/app/+service/service.routing.ts
@@ -2,8 +2,8 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ServiceComponent } from './service.component';
 
-const routes: Routes = [
+const ROUTES: Routes = [
     { path: '', component: ServiceComponent },
 ];
 
-export const ServiceRouting: ModuleWithProviders = RouterModule.forChild(routes);
+export const ServiceRouting: ModuleWithProviders = RouterModule.forChild(ROUTES);
