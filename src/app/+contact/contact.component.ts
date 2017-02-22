de561ccbd924975cc4ad6997142c9a9b4db8e4e5
Jordan Ide <jorrdandied@hotmail.com>
2017-02-22 02:30:16 +0000

    contact form submission

--- src/app/+contact/contact.component.ts
+++ src/app/+contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Http, Headers, RequestOptions } from '@angular/http';
 
 @Component({
   selector: 'sn-contact',
@@ -8,12 +9,35 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ContactComponent implements OnInit {
   data: {supportEmailAddress: string, contactNumber: string, businessAddress: string};
-  constructor(private route: ActivatedRoute) { }
+
+  model = {
+    name: '',
+    from: '',
+    message: '',
+    isDebug: true
+  };
+
+  submitted = false;
+
+  constructor(private route: ActivatedRoute, private http: Http) { }
 
   ngOnInit() {
     this.route.data.subscribe(({contact}: {contact: {supportEmailAddress: string, contactNumber: string, businessAddress: string}}) => {
       this.data = contact;
     });
   }
+
+  sendContact() {
+    this.submitted = true;
+    const headers = new Headers({ 'Content-Type': 'application/json'});
+    const options = new RequestOptions({ headers: headers });
+    this.http.post('https://ojrat822va.execute-api.eu-west-1.amazonaws.com/dev/contact-form', this.model, options)
+      .subscribe(() => {
+        console.log('Contact message sent');
+      });
+
+  }
+
+  get diagnostic() { return JSON.stringify(this.model); };
 
 }
--- src/app/+contact/contact.module.ts
+++ src/app/+contact/contact.module.ts
@@ -4,9 +4,13 @@ import { ContactComponent } from './contact.component';
 import { RouterModule } from '@angular/router';
 import { CONTACT_ROUTES } from './contact.routes';
 import { PageTitleModule } from '../shared/page-title/page-title.module';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   imports: [
+    HttpModule,
+    FormsModule,
     PageTitleModule,
     CommonModule,
     RouterModule.forChild(CONTACT_ROUTES)
