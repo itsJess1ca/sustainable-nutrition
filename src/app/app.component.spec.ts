39cf6ba21a09ec45df1de1d6b963a74c5cc14484
Jordan Ide <jorrdandied@hotmail.com>
2017-04-03 03:01:19 +0100

    remove unused imports

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,20 +1,4 @@
 /* tslint:disable: max-line-length */
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-
-import { AppComponent } from './app.component';
-import { NotFound404Component } from './not-found404.component';
-import { routes } from './app.routing';
-
-import 'rxjs/add/operator/takeUntil';
-import { SideNavComponent } from './side-nav/side-nav.component';
-import { FooterModule } from './footer/footer.module';
-import { BrandLogoModule } from './brand-logo/brand-logo.module';
-import { SimpleNotificationsModule } from 'angular2-notifications';
-import { GraphqlService } from './shared/graphql';
-import { HttpModule } from '@angular/http';
-import { ContentfulService } from './shared/contentful.service';
-
 describe('App Component', () => {
   it('should do math', () => {
     expect(1 + 1).toBe(2);
