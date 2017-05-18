bd04618d768d4355bdf1a13c1f58c256a6c8ee12
Jordan Ide <jorrdandied@hotmail.com>
2017-05-18 10:15:58 +0100

    remove dynamic prefixing

--- src/app/brand-logo/brand-logo.component.ts
+++ src/app/brand-logo/brand-logo.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'sn-brand-logo',
@@ -7,19 +6,7 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./brand-logo.component.css']
 })
 export class BrandLogoComponent {
-  @Input()
-  set prefix(val: string) {
-    this._prefix = val;
-  }
   @Input() theme: 'original' | 'green' = 'original';
-  public _prefix = 'sn';
-  get prefix() {
-    return this._prefix;
-  }
 
-  makeId(id: string) {
-    return `${this.prefix}-${id}`;
-  }
-
-  constructor(public sanitization: DomSanitizer) {}
+  constructor() {}
 }
